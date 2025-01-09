dc23249039c4bfb7f84dbf71be270ef2f5c2eaf8
Chaker KHACHINE <146932757+chaker-git@users.noreply.github.com>
2025-01-09 12:56:59 +0000

    add prints

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import (Flask, redirect, render_template, request,
 
 app = Flask(__name__)
 
+print("starting the app")
+
 
 @app.route('/')
 def index():
@@ -16,4 +18,5 @@ host = os.environ.get("HOST", "0.0.0.0")  # Default to '0.0.0.0' if not set
 port = os.environ.get("PORT", "50505")    # Default to '50505' if not set
 
 if __name__ == '__main__':
+   print("run app...")
    app.run(host=host, port=port)
