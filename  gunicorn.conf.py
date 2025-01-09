ca3da9d1148d76e9d75ca180d73aac47d50e9482
Chaker KHACHINE <146932757+chaker-git@users.noreply.github.com>
2025-01-09 12:54:27 +0000

    get host port dynamiclly

---  gunicorn.conf.py
+++  gunicorn.conf.py
@@ -6,7 +6,10 @@ max_requests_jitter = 50
 
 log_file = "-"
 
-bind = "0.0.0.0:50505"
+host = os.environ.get("HOST", "0.0.0.0")  # Default to '0.0.0.0' if not set
+port = os.environ.get("PORT", "50505")    # Default to '50505' if not set
+
+bind = f"{host}:{port}"
 
 workers = (multiprocessing.cpu_count() * 2) + 1
 threads = workers
--- app.py
+++ app.py
@@ -12,5 +12,8 @@ def index():
    return render_template('index.html')
 
 
+host = os.environ.get("HOST", "0.0.0.0")  # Default to '0.0.0.0' if not set
+port = os.environ.get("PORT", "50505")    # Default to '50505' if not set
+
 if __name__ == '__main__':
-   app.run()
+   app.run(host=host, port=port)
