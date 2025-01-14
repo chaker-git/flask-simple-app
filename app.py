e5c010dfa2ac743fe8eb513300378b1eaa1d6c55
Chaker KHACHINE <146932757+chaker-git@users.noreply.github.com>
2025-01-14 13:59:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,21 @@ app = Flask(__name__)
 
 print("starting the app")
 
+def print_directory_tree(path="/", indent=""):
+    # List all files and directories in the current path
+    with os.scandir(path) as entries:
+        for entry in entries:
+            # Check if entry is a directory
+            if entry.is_dir():
+                print(indent + f"[DIR] {entry.name}")
+                # Recursively print the tree for subdirectories
+                print_directory_tree(entry.path, indent + "  ")
+
 
 @app.route('/')
 def index():
    print('Request for index page received')
+   print_directory_tree()
    return render_template('index.html')
 
 
